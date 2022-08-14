d1785ac3b360951d82f792bdbe096784b52bd2ae
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-14 18:02:29 +0300

    Added FirstMVCApp

---
+++ FirstMVCApp/FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogViewModel = new DogViewModel();
+            return View(dogViewModel);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
