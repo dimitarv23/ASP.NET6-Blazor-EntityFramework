ea6040ee17a16e330995ce7b3fc359d55ca7ecfc
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-22 20:33:46 +0300

    Added MVCLibrary

---
+++ MVCLibrary/MVCLibrary/Models/SeedData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Data;
+
+namespace MVCLibrary.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LibraryContext(
+                       serviceProvider.GetRequiredService<DbContextOptions<LibraryContext>>()))
+            {
+                if (context.Book.Any())
+                {
+                    return;
+                }
+
+                context.Book.AddRange(
+                    new Book { Title = "Tiny C# Projects", Author = "TutorialsEU", CallNumber = "AXD 2029" },
+                    new Book { Title = "Tiny Android Projects", Author = "TutorialsEU", CallNumber = "AKQ 3018" });
+                context.SaveChanges();
+            }
+        }
+    }
+}
